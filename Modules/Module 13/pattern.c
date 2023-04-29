9236b8774fb6687057cd5cb01ca04fbcb1bd6bb5
Dipon11 <dipondas1132@gmail.com>
2023-04-29 22:26:30 +0600

    Unproductive

---
+++ Modules/Module 13/pattern.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+int n,k=1;
+scanf("%d",&n);
+for (int i = 1; i < n; i++)// koi bar kaj tah korbe
+{
+    for (int j = 1; j <= k; j++)
+    {
+        printf("*");
+                               //kaj
+    }
+   printf("\n");
+    k++;
+}
+
+    return 0;
+}
+// *
+// **
+// ***
+// ****
